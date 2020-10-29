079a3e35cc30159badfccc66f507fa05cbcaed25
3milychu@gmail.com <3milychu@gmail.com>
2020-10-28 22:05:49 -0400

    week9

---
+++ week9/main.js
@@ -0,0 +1,3 @@
+//I didn't write my script here, 
+//but I could paste everything between the script tags from the index.html here
+//and with it linked in my <head> tag, the javascript will run the same
