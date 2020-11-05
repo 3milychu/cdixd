e3a431556caf92b29e0bbe1e237fd6254a84ad0b
3milychu@gmail.com <3milychu@gmail.com>
2020-11-04 20:06:04 -0500

    push

--- week10/main.js
+++ week10/main.js
@@ -4,10 +4,12 @@ let selection = 0
 function update() {
 	// local variables
 	let options = document.querySelectorAll('.option')
+	let title = document.querySelector('h1')
 	console.log(options)
 	options.forEach(item => item.onclick=function() {
 		selection = item.id
 		console.log(selection)
+		title.innerHTML= "I am feeling " + selection
 	})
 }
 
